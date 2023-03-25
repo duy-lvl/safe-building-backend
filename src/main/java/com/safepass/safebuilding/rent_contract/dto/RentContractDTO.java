426a847eb63b4a5ba363d2dd1589ec84924ac98c
Lenny-TLPL <tranlephilong27@gmail.com>
2023-03-25 07:56:17 +0700

    add serializable

--- src/main/java/com/safepass/safebuilding/rent_contract/dto/RentContractDTO.java
+++ src/main/java/com/safepass/safebuilding/rent_contract/dto/RentContractDTO.java
@@ -5,11 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class RentContractDTO {
+public class RentContractDTO  implements Serializable{
     private String id;
     private String buildingName;
     private String buildingAddress;
